d6d280ca605dc393b9f519a0581af3ba8b028b90
Matias Seltzer <107654917+MattSeltz@users.noreply.github.com>
2025-10-21 19:51:31 -0300

    Se añadió manejo de usuarios conectados

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h1>Chat</h1>;
+  const [usersList, setUsersList] = useState([]);
+
+  useEffect(() => {
+    const socketInstance = io(process.env.NEXT_PUBLIC_SERVER, {
+      withCredentials: true,
+    });
+
+    socketInstance.on("usersList", (res) => setUsersList(res));
+
+    return () => {
+      socketInstance.off("usersList");
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <p className="text-3xl">
+        Usuarios conectados: <strong>{usersList.length}</strong>
+      </p>
+    </main>
+  );
 }
