d1dc170be426c8933244b8a1fbbb4cc1ce41415d
Matias Seltzer <107654917+MattSeltz@users.noreply.github.com>
2025-10-23 17:45:47 -0300

    Cambio de UI implementado

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -50,8 +50,8 @@ export default function Home() {
   };
 
   return (
-    <main className="h-screen flex flex-col justify-center items-center gap-8 relative">
-      <p className="text-3xl">
+    <main className="h-screen flex gap-8 p-8 relative">
+      {/* <p className="text-3xl">
         Connected users: <strong>{usersList.length}</strong>
       </p>
 
@@ -68,7 +68,56 @@ export default function Home() {
         ) : (
           <p>No user has logged in yet</p>
         )}
-      </ul>
+      </ul> */}
+
+      <section className="shadow shadow-white rounded h-[95%] w-1/4 p-4 flex flex-col items-center gap-8">
+        <button
+          type="button"
+          className="bg-white text-black font-bold rounded px-4 py-2 cursor-pointer transition-opacity hover:opacity-50 mt-8"
+        >
+          New chat
+        </button>
+
+        <ul className="p-4">
+          <li className="text-xl transition-shadow hover:shadow shadow-white cursor-pointer rounded px-4 py-2">
+            Marcus
+          </li>
+          <li className="text-xl transition-shadow hover:shadow shadow-white cursor-pointer rounded px-4 py-2">
+            Max
+          </li>
+        </ul>
+      </section>
+
+      <section className="shadow shadow-white rounded h-[95%] w-3/4 p-4 flex flex-col justify-between">
+        <header className="flex justify-between shadow shadow-white rounded p-4">
+          <p>Online</p>
+
+          <p>Marcus</p>
+        </header>
+
+        <ul className="flex flex-col gap-8 overflow-auto py-4">
+          <li className="bg-white text-black font-bold rounded px-4 py-2 self-start">
+            Hola
+          </li>
+          <li className="bg-white text-black font-bold rounded px-4 py-2 self-end">
+            Hola
+          </li>
+          <li className="bg-white text-black font-bold rounded px-4 py-2 self-start">
+            ¿Cómo estas?
+          </li>
+          <li className="bg-white text-black font-bold rounded px-4 py-2 self-end">
+            Bien, ¿Y vos?
+          </li>
+          <li className="bg-white text-black font-bold rounded px-4 py-2 self-start">
+            Bien
+          </li>
+        </ul>
+        <input
+          type="text"
+          placeholder="Message..."
+          className="shadow shadow-white rounded px-4 py-2 w-full"
+        />
+      </section>
 
       <button
         type="button"
